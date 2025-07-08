133fab2418300818289a2a0cdbfbdc859407b035
Abeleops <abelejohnjuarez03@gmail.com>
2025-07-08 11:02:46 +0800

    feat: new image + refactor css + new function in javascript

--- pages/hero-3/assets/js/scripts.js
+++ pages/hero-3/assets/js/scripts.js
@@ -1,12 +1,24 @@
- const btn2 = document.querySelector(".btn2");
+const btn1 = document.querySelector(".btn1");
+    const btn2 = document.querySelector(".btn2");
     const sanji = document.querySelector(".sanji");
     const sanjiKick = document.querySelector("#first");
+    const sanjiLeft = document.querySelector("#sanji-left");
+
+    btn1.addEventListener("mouseenter", () => {
+      sanji.style.display = "none";
+      sanjiLeft.style.display = "block";
+      sanjiKick.style.display = "none";
+    });
+    btn1.addEventListener("mouseleave", () => {
+      sanji.style.display = "block";
+      sanjiLeft.style.display = "none";
+    });
 
     btn2.addEventListener("mouseenter", () => {
       sanji.style.display = "none";
       sanjiKick.style.display = "block";
+      sanjiLeft.style.display = "none";
     });
-
     btn2.addEventListener("mouseleave", () => {
       sanji.style.display = "block";
       sanjiKick.style.display = "none";
