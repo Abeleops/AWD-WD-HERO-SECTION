96a6c468540f287268f27222a078475228e75ee0
Abeleops <abelejohnjuarez03@gmail.com>
2025-07-08 10:49:46 +0800

    fix:sanji_kick image

--- pages/hero-3/assets/js/scripts.js
+++ pages/hero-3/assets/js/scripts.js
@@ -1,13 +1,13 @@
-const btn2 = document.querySelector('.btn2');
-const sanji = document.querySelector('.sanji');
-const sanjiRight = document.querySelector('#first');
+ const btn2 = document.querySelector(".btn2");
+    const sanji = document.querySelector(".sanji");
+    const sanjiKick = document.querySelector("#first");
 
-btn2.addEventListener('mouseenter', () => {
-  sanji.style.opacity = '0';
-  sanjiRight.style.opacity = '1';
-});
+    btn2.addEventListener("mouseenter", () => {
+      sanji.style.display = "none";
+      sanjiKick.style.display = "block";
+    });
 
-btn2.addEventListener('mouseleave', () => {
-  sanji.style.opacity = '1';
-  sanjiRight.style.opacity = '0';
-});
+    btn2.addEventListener("mouseleave", () => {
+      sanji.style.display = "block";
+      sanjiKick.style.display = "none";
+    });
